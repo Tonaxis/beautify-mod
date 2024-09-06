3f97805ba980a5d12ab4f00d2ddb95148c735191
Tonaxis <83670267+Tonaxis@users.noreply.github.com>
2024-09-07 01:20:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,19 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const beautify_dev_1 = require("beautify-dev");
-(() => __awaiter(void 0, void 0, void 0, function* () {
-    const extractor = {
-        name: "images",
-        extract: (document) => {
-            return document.querySelectorAll("img");
-        },
-    };
-    const result = (0, beautify_dev_1.extract)([extractor]);
-    console.log("result", result);
-    const result2 = yield (0, beautify_dev_1.extract)([extractor]);
-    console.log("PROMISE result", result2);
-}))();
+const extractor = {
+    name: "images",
+    extract: (document) => {
+        return document.querySelectorAll("img");
+    },
+};
+const result = (0, beautify_dev_1.extract)([extractor]);
+console.log("result", result);
+// const result2 = await extract([extractor]);
+// console.log("PROMISE result", result2);
+// (async () => {
+// })();
 
 },{"beautify-dev":3}],2:[function(require,module,exports){
 "use strict";

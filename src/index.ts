21843934b511e1ef1f6c7f77b9b28759ec941d4a
Tonaxis <83670267+Tonaxis@users.noreply.github.com>
2024-09-08 00:20:20 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 import { extract, render } from "beautify-dev";
 import extractors from "./extractors";
 
-console.log("ENTERED 2");
-
 const results = extract(extractors);
-
-console.log("doc", (globalThis as any).beautify.document.outerHTML);
 
 results.divs.forEach((element) => {
   element.style.boxShadow = "1px 2px 3px 4px red";

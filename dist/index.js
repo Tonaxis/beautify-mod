5c6cd373bbffe651758b87de55f8a6ef3cc89321
Tonaxis <83670267+Tonaxis@users.noreply.github.com>
2024-09-08 00:10:21 +0200

    update

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ const beautify_dev_1 = require("beautify-dev");
 const extractors_1 = __importDefault(require("./extractors"));
 console.log("ENTERED 2");
 const results = (0, beautify_dev_1.extract)(extractors_1.default);
-console.log("results", results);
+console.log("doc", globalThis.beautify.document.outerHTML);
 results.divs.forEach((element) => {
     element.style.boxShadow = "1px 2px 3px 4px red";
 });
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ console.log("ENTERED 2");
 
 const results = extract(extractors);
 
-console.log("results", results);
+console.log("doc", (globalThis as any).beautify.document.outerHTML);
 
 results.divs.forEach((element) => {
   element.style.boxShadow = "1px 2px 3px 4px red";

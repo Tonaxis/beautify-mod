0e20d21628c481ea73e716f1fcfa5082d66fda26
Tonaxis <83670267+Tonaxis@users.noreply.github.com>
2024-09-08 12:12:36 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import extractors from "./extractors";
 
 const results = extract(extractors);
 
-results.divs.forEach((element) => {
+results.divs?.forEach((element) => {
   element.style.boxShadow = "1px 2px 3px 4px red";
 });
 
